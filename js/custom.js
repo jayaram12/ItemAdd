c4fa7f0ce3af465d7c1b9e881790e3600f96f348
jayaram12 <jayarammasetti@gmail.com>
2018-10-25 16:28:01 +0530

    files are hosting

---
+++ js/custom.js
@@ -0,0 +1,83 @@
+$(document).ready(function(){
+  /* Tooltip  */
+  $('[data-toggle="tooltip"]').tooltip({
+    trigger: 'hover'
+  });
+// starting the script on page load Image Preview
+imagePreview();
+
+// $('.collections .table tbody tr').click(function(){
+//   $(this).toggleClass('selected');
+// });
+
+// For select 2
+$(".searchbykeyword").select2();
+$('.selectpicker').selectpicker();
+
+
+$('#AddNewCollection').hide();
+$('#AddNewCollectionBtn').click(function(){
+  $('#AddNewCollection').slideDown('slow');
+});
+
+$('#Results').hide();
+$('#Filter').click(function(){
+  $('#Results').slideDown('slow');
+});
+
+
+$('.CellEditBtn').click(function(){
+    $('.CellEdit').attr('contenteditable', 'true');
+});
+$('.CellEditBtn1').click(function(){
+    $('.CellEdit1').attr('contenteditable', 'true');
+});
+$('.CellEditBtn2').click(function(){
+    $('.CellEdit2').attr('contenteditable', 'true');
+});
+
+
+});
+
+this.imagePreview = function(){
+/* CONFIG */
+xOffset = 5;
+yOffset = 15;
+// these 2 variable determine popup's distance from the cursor
+// you might want to adjust to get the right result
+/* END CONFIG */
+$("a.preview").hover(function(e){
+this.t = this.title;
+this.title = "";
+var c = (this.t != "") ? "<br/>" + this.t : "";
+$("body").append("<p id='ImagePreview'><img src='"+ this.href +"' />"+ c +"</p>");
+$("#ImagePreview")
+.css("top",(e.pageY - xOffset) + "px")
+.css("left",(e.pageX + yOffset) + "px")
+.fadeIn("fast");
+},
+function(){
+this.title = this.t;
+$("#ImagePreview").remove();
+});
+
+};
+
+
+$("document").ready(function(){
+  $(".tab-slider--body").hide();
+  $(".tab-slider--body:first").show();
+});
+
+$(".tab-slider--nav li").click(function() {
+  $(".tab-slider--body").hide();
+  var activeTab = $(this).attr("rel");
+  $("#"+activeTab).fadeIn();
+	if($(this).attr("rel") == "tab2"){
+		$('.tab-slider--tabs').addClass('slide');
+	}else{
+		$('.tab-slider--tabs').removeClass('slide');
+	}
+  $(".tab-slider--nav li").removeClass("active");
+  $(this).addClass("active");
+});
